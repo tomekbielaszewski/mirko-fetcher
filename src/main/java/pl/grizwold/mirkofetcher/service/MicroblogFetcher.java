97a54c4618c4167217f0aa14054ba9a143247519
Grizwold <tomekbielaszewski@gmail.com>
2017-08-05 15:55:14 +0200

    Fetcher simplified and now it is possible to run concurrent instances. Low chance for overwriting with old data

--- src/main/java/pl/grizwold/mirkofetcher/service/MicroblogFetcher.java
+++ src/main/java/pl/grizwold/mirkofetcher/service/MicroblogFetcher.java
@@ -1,31 +1,51 @@
 package pl.grizwold.mirkofetcher.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.grizwold.microblog.model.Entry;
 import pl.grizwold.mirkofetcher.model.repo.EntryRepository;
+import pl.grizwold.mirkofetcher.wykop.client.MicroblogClient;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @Slf4j
 @Service
 public class MicroblogFetcher {
     @Autowired
-    private EntryProvider entryProvider;
+    private MicroblogClient microblog;
 
     @Autowired
     private EntryRepository entryRepository;
 
-    public void run(long until) {
-        log.info("Getting microblog entries...");
-        Set<Entry> entries = entryProvider.getEntries(until);
+    public void run(long untilTimestamp) {
+        DateTime timeOffset = new DateTime(untilTimestamp);
+        log.info("Started Microblog Fetcher and getting entries until {}", timeOffset);
 
-        Set<Long> ids = entries.stream().map(Entry::getId).collect(Collectors.toSet());
-        log.info("Unique IDs: " + ids.size());
-
-        log.info("Saving {} microblog entries in DB...", entries.size());
+        log.info("Getting and saving first page...");
+        List<Entry> entries = microblog.firstPage();
         entryRepository.save(entries);
+
+        do {
+            Entry lastEntry = getLastEntry(entries);
+            log.info("Getting entries before {} until {} and saving them", lastEntry.getDateAdded(), timeOffset);
+            entries = microblog.byFirstId(lastEntry.getId());
+            entryRepository.save(entries);
+        } while (!hasOldEntries(entries, untilTimestamp));
+
+        log.info("Reached the desired time offset {}", timeOffset);
+    }
+
+    private Entry getLastEntry(List<Entry> entries) {
+        return entries.get(entries.size() - 1);
+    }
+
+    private boolean hasOldEntries(List<Entry> entries, long untilTimestamp) {
+        return entries.stream().anyMatch(e -> this.isOld(e, untilTimestamp));
+    }
+
+    private boolean isOld(Entry entry, long untilTimestamp) {
+        return entry.getDateAdded().getMillis() < untilTimestamp;
     }
 }
